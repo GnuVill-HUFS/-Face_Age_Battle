01a2d3b5ddadf1b68bcd341ee3dd457efde89d27
jenny6585 <39253236+jenny6585@users.noreply.github.com>
2018-05-21 12:44:04 +0000

    Update views.py

--- face_battle/views.py
+++ face_battle/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     render(request,'main.html')
+    
+def single_mode(request):
+    return render(request,'single_mode.html')
+
+def dual_mode(request):   
+    return render(request,'dual_mode.html')
