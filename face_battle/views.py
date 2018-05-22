9f0a191851091f76b5c475e48acee5be9c3ef0ec
SeunghyeonJeon <37951608+SeunghyeonJeon@users.noreply.github.com>
2018-05-22 16:22:59 +0900

    Update views.py

--- face_battle/views.py
+++ face_battle/views.py
@@ -11,3 +11,27 @@ def dual_mode(request):
 
 def mod_select(request):
     return render(request, 'mode_select.html')
+
+
+
+def single_result(requests):
+
+    picture =['pics']
+
+    image_info = imageFind(picture)
+    image_info = {'sex': None}
+
+
+    image_info['age']=int(image_info['age'])
+
+
+    if(image_info['male']>image_info['female']):
+        image_info['sex']='남자'
+    else if(image_info['male']<image_imfo['female']):
+        image_info['sex']='여자'
+    else:
+        image_info['sex']='당첨'
+
+
+    return render(requests, 'single_mode.html', image_info)
+
