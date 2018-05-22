f26bdf528ea4f631f1df71df471eab14f6028c20
HongChanHyeok <hchc5783@naver.com>
2018-05-22 23:40:27 +0900

    완성!~!

--- face_battle/face_api.py
+++ face_battle/face_api.py
@@ -1,25 +1,49 @@
-def imageFind(requests):
+from PIL import Image, ImageFilter
+import sys
+import requests
+import argparse
+from collections import ChainMap
 
 
-    face_info = {}
-    face_info['age']=None
-    face_info['male']=None
-    face_info['female']=None
-    face_info['celebrity']=None
-    """
-    requests = ['image':이미지로컬저장위치] 라고 저장되어있음
+API_URL = 'https://kapi.kakao.com/v1/vision/face/detect'
+MYAPP_KEY = '5da57c0ae4fcf77e767e2df44ca0bd62'
 
-    KAKAO 비전API의 얼굴인식을 사용합니다.
+def detect_face(filename):
+    headers = {'Authorization' : 'KakaoAK {}' .format(MYAPP_KEY)}
 
-    face_info 변수에
-    [나이정보, 성별 퍼센트, 닮은꼴]
-    순서대로 age male female celebrity
-    이 필요합니다.
+    try :
+        files = { 'file' : open(filename, 'rb')}
+        resp = requests.post(API_URL, headers=headers, files=files)
+        resp.raise_for_status()
+        return resp.json()
+    except Exception as e:
+        print(str(e))
+        sys.exit(0)
 
-    ** 선택사항
-    PIL 라이브러리 이용하여 requests로 받은 이미지에 얼굴위치를
-    (선으로) 적용시켜 사람들이 눈으로 볼 수 있게끔 이미지처리를 한다.
-    """
+def mosaic(filename, detection_result):
+    image = Image.open(filename)
+    return image
 
+def imageFind(requests):#request는 받는 사진 을 뜻함 "~.jpg"형식으로 받아야함 더블쿼테이션 필수
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser(description='Mosaic faces.')
+        parser.add_argument('image_file', type=str, nargs='?', default=requests,
+                            help='image file to hide faces')
+        args = parser.parse_args()
+        detection_result = detect_face(args.image_file)
 
-    return face_info
+        a={}
+        a=detection_result['result']['faces']
+        b= ChainMap(*a)
+        c=b['facial_attributes']['gender']
+        male=c['male']
+        female=c['female']
+        d=b['facial_attributes']
+        age=d['age']
+        score=b['score']
+
+        face_info={'male':male, 'female':female,'age':age,'score':score}
+        return(face_info)
+#result,faces,facial_attribute,gender male 또는 female
+
+#찬혁
